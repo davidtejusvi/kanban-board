083528d6facbc3d4788498fd4c3b9320f5479836
david <davidtejusvi@gmail.com>
2024-07-22 21:11:29 +0530

    css for kanban board columns

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,22 @@ function App() {
 
   return (
     <>
-      <div className="app-container">
-        kanban board
+      <div className='board'>
+        <div className='column'>
+          <div className="column-header">To Do</div>
+          <div className="task">Task 1</div>
+          <div className="task">Task 2</div>
+        </div>
+        <div className='column'>
+          <div className="column-header">In Progress</div>
+          <div className="task">Task 3</div>
+        </div>
+        <div className='column'>
+          <div className="column-header">Done</div>
+          <div className="task">Task 4</div>
+        </div>
       </div>
+
     </>
   )
 }
